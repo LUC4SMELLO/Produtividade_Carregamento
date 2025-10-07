8746803447061506f511e5d8793131eea58e9505
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-07 16:52:55 -0300

    atualizando html

---
+++ backend/contantes/caminhoes.py
@@ -0,0 +1,87 @@
+caminhoes = [
+    {
+        "caminhao": 17,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5", "A6"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5", "M6"],
+    },
+    {
+        "caminhao": 18,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5", "A6", "A7"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5", "M6", "M7"],
+    },
+    {
+        "caminhao": 19,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5", "A6", "A7"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5", "M6", "M7"],
+    },
+    {
+        "caminhao": 20,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 21,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 22,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 23,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 24,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 25,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 26,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 27,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 28,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 29,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 30,
+        "baias_a": ["A1", "A2", "A3", "A4", "A5"],
+        "baias_m": ["M1", "M2", "M3", "M4", "M5"]
+    },
+    {
+        "caminhao": 31,
+        "baias_a": ["A1", "A2", "A3", "A4"],
+        "baias_m": ["M1", "M2", "M3", "M4"]
+    },
+    {
+        "caminhao": 32,
+        "baias_a": ["A1", "A2", "A3", "A4"],
+        "baias_m": ["M1", "M2", "M3", "M4"]
+    },
+    {
+        "caminhao": 33,
+        "baias_a": ["A1", "A2", "A3"],
+        "baias_m": ["M1", "M2", "M3"]
+    },
+]
